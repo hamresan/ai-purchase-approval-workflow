6aeaf26efe893513552f75c43ce26cec4ed8bded
hamresan <pourshahmir@gmail.com>
2026-09-25 18:54:58 +0400

    feat: add trusted purchase tools and policies

---
+++ backend/src/ai_purchase_workflow/application/purchase_requests/preparation.py
@@ -0,0 +1,55 @@
+from uuid import UUID
+
+from ai_purchase_workflow.application.purchase_requests.dto import PurchaseRequestView
+from ai_purchase_workflow.application.purchase_requests.repository import PurchaseRequestRepository
+from ai_purchase_workflow.application.purchase_requests.trusted_tools import (
+    CheckBudget,
+    CreateDraftOrder,
+    FindVendor,
+    SubmitOrder,
+)
+from ai_purchase_workflow.application.purchase_requests.use_cases import PurchaseRequestNotFoundError
+from ai_purchase_workflow.domain.purchase_requests import RequestStatus
+
+
+class PreparePurchaseRequest:
+    def __init__(
+        self,
+        repository: PurchaseRequestRepository,
+        find_vendor: FindVendor,
+        create_draft_order: CreateDraftOrder,
+        check_budget: CheckBudget,
+    ) -> None:
+        self._repository = repository
+        self._find_vendor = find_vendor
+        self._create_draft_order = create_draft_order
+        self._check_budget = check_budget
+
+    async def execute(self, request_id: UUID) -> PurchaseRequestView:
+        request = await self._repository.get(request_id)
+        if request is None:
+            raise PurchaseRequestNotFoundError(request_id)
+
+        trusted_items = tuple([await self._find_vendor.execute(item) for item in request.items])
+        draft = self._create_draft_order.execute(request, trusted_items)
+        await self._check_budget.execute(request.requester_name, draft.total)
+        request.draft_order = draft
+        request.transition_to(RequestStatus.PENDING_APPROVAL)
+        await self._repository.save(request)
+        return PurchaseRequestView.from_domain(request)
+
+
+class SubmitPurchaseRequest:
+    def __init__(self, repository: PurchaseRequestRepository, submit_order: SubmitOrder) -> None:
+        self._repository = repository
+        self._submit_order = submit_order
+
+    async def execute(self, request_id: UUID) -> PurchaseRequestView:
+        request = await self._repository.get(request_id)
+        if request is None:
+            raise PurchaseRequestNotFoundError(request_id)
+
+        await self._submit_order.execute(request)
+        request.transition_to(RequestStatus.SUBMITTED)
+        await self._repository.save(request)
+        return PurchaseRequestView.from_domain(request)
